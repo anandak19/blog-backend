eb6ca1ec9cf1f6e3249c27ec9b967e3437aadc13
Anandakrishnan H <anandak19h@gmail.com>
2026-03-27 02:05:18 +0530

    update port

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { appConfig } from "./config/app.config";
 import connectDB from "./config/db.config";
 
 connectDB();
-app.listen(appConfig.PORT, () => {
-  console.log(`Server running at port ${appConfig.PORT}`);
-});
+app.listen(appConfig.PORT, '0.0.0.0')
+// app.listen(appConfig.PORT,  () => {
+//   console.log(`Server running at port ${appConfig.PORT}`);
+// });
